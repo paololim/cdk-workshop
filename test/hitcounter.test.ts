c07527286513b8041526054cc27be09705910705
Paolo Lim <paolo@dynamic.xyz>
2022-06-16 11:43:19 -0400

    save

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -51,3 +51,22 @@ test('Lambda Has Environment Variables', () => {
     }
   );
 });
+
+test('DynamoDB Table Created With Encryption', () => {
+  const stack = new cdk.Stack();
+  // WHEN
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream:  new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
+  });
+  // THEN
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    SSESpecification: {
+      SSEEnabled: true
+    }
+  });
+});
